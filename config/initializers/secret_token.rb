88aec7baabd3d2757c31b71c31bb53360798a5db
David Ohayon <ohayon.1@gmail.com>
2012-11-15 17:48:19 -0500

    first of many pushes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BssWallet::Application.config.secret_token = '6d54b5c03fbf3eac29440a4e088ad1bcaf3ccbd138195f87853a199eb17136854db687648e6c219ecd0323cdd6253a06e0258ce70330fb9a0d607ec8638de5c8'
