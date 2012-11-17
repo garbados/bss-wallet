7b3b3f45a0403e7ef7ed63100ef2230550e4cfac
David Ohayon <ohayon.1@gmail.com>
2012-11-17 17:55:30 -0500

    validating numericality of zip code

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,6 +6,7 @@ class Merchant < ActiveRecord::Base
     validates :rep_first_name, :rep_last_name, :length => { :maximum => 50 }
     #validates :email, :email_format => {:message => "not valid"}
     validates :address_st_1, :address_st_2, :length => { :maximum => 100 }
+    validates :zip, :numericality => true
     validates_format_of :zip,
                         :with => %r{\d{5}(-\d{4})?},
                         :message => "should be 12345 or 12345-1234"
