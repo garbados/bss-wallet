e2b782e21afbc35729b83ed46359c4d4f52fc26b
David Ohayon <ohayon.1@gmail.com>
2012-11-17 22:22:07 -0500

    typo fix in offers_controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -28,23 +28,23 @@ class MerchantsController < ApplicationController
 	def create
 		@merchant = Merchant.new
 		@merchant.store_name = params[:merchant][:store_name]
-        @merchant.rep_first_name = params[:merchant][:rep_first_name]
-        @merchant.rep_last_name = params[:merchant][:rep_last_name]
-        @merchant.email = params[:merchant][:email]
-        @merchant.address_st_1 = params[:merchant][:address_st_1]
-        @merchant.address_st_2 = params[:merchant][:address_st_2]
-        @merchant.city = params[:merchant][:city]
-        @merchant.state = params[:merchant][:state]
-        @merchant.zip = params[:merchant][:zip]
+    @merchant.rep_first_name = params[:merchant][:rep_first_name]
+    @merchant.rep_last_name = params[:merchant][:rep_last_name]
+    @merchant.email = params[:merchant][:email]
+    @merchant.address_st_1 = params[:merchant][:address_st_1]
+    @merchant.address_st_2 = params[:merchant][:address_st_2]
+    @merchant.city = params[:merchant][:city]
+    @merchant.state = params[:merchant][:state]
+    @merchant.zip = params[:merchant][:zip]
        	
-       	respond_to do |format|
-      		if @merchant.save
-		        format.html { redirect_to @merchant, notice: 'merchant was successfully created.' }
-		        format.json { render json: @merchant, status: :created, location: @merchant }
-      		else
-		        format.html { render action: "new" }
-		        format.json { render json: @merchant.errors, status: :unprocessable_entity }
-      		end
-   		 end
+   	respond_to do |format|
+  		if @merchant.save
+        format.html { redirect_to @merchant, notice: 'merchant was successfully created.' }
+        format.json { render json: @merchant, status: :created, location: @merchant }
+  		else
+        format.html { render action: "new" }
+        format.json { render json: @merchant.errors, status: :unprocessable_entity }
+  		end
+		 end
 	end
 end
